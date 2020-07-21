011a923283ba436e950030286beb43cf952ec2ea
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-21 22:22:45 +0200

    Add redirect if user is logged in

--- src/components/forms/login.js
+++ src/components/forms/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -66,64 +66,69 @@ class Login extends Component {
 
     render() {
         return (
-            <div className='first-color-bg'>
-                <Row>
-                    <Col xs={1} md={3} lg={4}></Col>
-                    <Col xs={10} md={6} lg={4}>
-                        <Container className='form-container pt-1 pb-1'>
-                            <hr className='mx-5' />
-                            <Row className='justify-content-center'>
-                                <Link to='/' >
-                                    <Image src={logo} width='100' className='link-text img-shadow'></Image>
-                                </Link>
-                            </Row>
-                            <Row className='justify-content-center mb-3'>
-                                <h2 className='text-center grey-text'>LOGOWANIE</h2>
-                            </Row>
-                            <Form onSubmit={this.handleLogin}>
-                                <Form.Group as={Row} className='mx-5'>
-                                    <Form.Label column sm={3} className='grey-text'><b>login</b></Form.Label>
-                                    <Col sm={9}>
-                                        <Form.Control
-                                            type='text'
-                                            placeholder='login...'
-                                            name='username'
-                                            value={this.state.username}
-                                            onChange={this.onChangeUsername}
-                                        />
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group as={Row} className='mx-5'>
-                                    <Form.Label column sm={3} className='grey-text'><b>hasło</b></Form.Label>
-                                    <Col sm={9}>
-                                        <Form.Control
-                                            type='password'
-                                            placeholder='hasło...'
-                                            name='password'
-                                            value={this.state.password}
-                                            onChange={this.onChangePassword}
-                                        />
-                                    </Col>
-                                </Form.Group>
-                                <Form.Group as={Row} className='d-flex justify-content-center'>
-                                    <Button
-                                        type='submit'
-                                        variant='outline-dark'
-                                        className='mb-2'
-                                        disabled={this.state.loading}
-                                    >
-                                        Zaloguj
+            <div>
+                { localStorage.getItem('token') && (
+                    <Redirect to="/logged" />
+                ) }
+                <div className='first-color-bg'>
+                    <Row>
+                        <Col xs={1} md={3} lg={4}></Col>
+                        <Col xs={10} md={6} lg={4}>
+                            <Container className='form-container pt-1 pb-1'>
+                                <hr className='mx-5' />
+                                <Row className='justify-content-center'>
+                                    <Link to='/' >
+                                        <Image src={logo} width='100' className='link-text img-shadow'></Image>
+                                    </Link>
+                                </Row>
+                                <Row className='justify-content-center mb-3'>
+                                    <h2 className='text-center grey-text'>LOGOWANIE</h2>
+                                </Row>
+                                <Form onSubmit={this.handleLogin}>
+                                    <Form.Group as={Row} className='mx-5'>
+                                        <Form.Label column sm={3} className='grey-text'><b>login</b></Form.Label>
+                                        <Col sm={9}>
+                                            <Form.Control
+                                                type='text'
+                                                placeholder='login...'
+                                                name='username'
+                                                value={this.state.username}
+                                                onChange={this.onChangeUsername}
+                                            />
+                                        </Col>
+                                    </Form.Group>
+                                    <Form.Group as={Row} className='mx-5'>
+                                        <Form.Label column sm={3} className='grey-text'><b>hasło</b></Form.Label>
+                                        <Col sm={9}>
+                                            <Form.Control
+                                                type='password'
+                                                placeholder='hasło...'
+                                                name='password'
+                                                value={this.state.password}
+                                                onChange={this.onChangePassword}
+                                            />
+                                        </Col>
+                                    </Form.Group>
+                                    <Form.Group as={Row} className='d-flex justify-content-center'>
+                                        <Button
+                                            type='submit'
+                                            variant='outline-dark'
+                                            className='mb-2'
+                                            disabled={this.state.loading}
+                                        >
+                                            Zaloguj
                                         {this.state.loading && (
-                                            <Spinner animation='border' variant='secondary' size='sm' className='ml-2' />
-                                        )}
-                                    </Button>
-                                </Form.Group>
-                            </Form>
-                            <hr className='mx-5' />
-                        </Container>
-                    </Col>
-                    <Col xs={1} md={3} lg={4}></Col>
-                </Row>
+                                                <Spinner animation='border' variant='secondary' size='sm' className='ml-2' />
+                                            )}
+                                        </Button>
+                                    </Form.Group>
+                                </Form>
+                                <hr className='mx-5' />
+                            </Container>
+                        </Col>
+                        <Col xs={1} md={3} lg={4}></Col>
+                    </Row>
+                </div>
             </div>
         );
     }
