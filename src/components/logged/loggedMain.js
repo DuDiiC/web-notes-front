9fcc94c443e34ddc6988b227b6acc9821ba367f1
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-22 14:11:11 +0200

    Create test request for current user

--- src/components/logged/loggedMain.js
+++ src/components/logged/loggedMain.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import AuthService from './../../services/authService';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
+import { Container } from 'react-bootstrap';
+import UserService from './../../services/userService';
 
 class LoggedMain extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                noteIds: []
+            },
+        };
+    }
+
+    componentDidMount() {
+        UserService.getCurrentUser()
+            .then((response) => {
+                const user = response.data;
+                this.setState({ user });
+            })
+            .catch(error => { console.log(error) });
+    }
+
     render() {
         return (
             <div>
@@ -13,6 +32,15 @@ class LoggedMain extends Component {
                 )}
                 <Container className='text-center'>
                     <h1 className='m-5 p-5'>zalogowano</h1>
+                    <h3>
+                        {this.state.user.id}
+                    </h3>
+                    <h5>
+                        {this.state.user.username}
+                    </h5>
+                    {this.state.user.noteIds.map(function (id) {
+                        return <li>{id}</li>
+                     }) }
                 </Container>
             </div>
         )
