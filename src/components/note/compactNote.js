c08637b6bf4191405015285b05a5d2ad649c5810
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-22 16:14:58 +0200

    Add note components skeletons

---
+++ src/components/note/compactNote.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CompactNote extends Component {
+    render() {
+        return(
+            <div>
+                compact note
+            </div>
+        )
+    };
+}
