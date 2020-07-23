79ad6d342388be24c74e274b6bdb981ca6346177
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-23 19:18:46 +0200

    Create simple note layout

--- src/components/note/note.js
+++ src/components/note/note.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
 
 class Note extends Component {
     render() {
         return(
-            <div>
-                note
-            </div>
+            <Container className='note-border m-3 p-3'>
+                <p>{this.props.note.createdAt}</p>
+                <h3>{this.props.note.tile}</h3>
+                <hr className='mt-2 mb-4 p-0' />
+                <ReactMarkdown source={this.props.note.content} />
+            </Container>
         )
     };
 }
+
+export default Note;
