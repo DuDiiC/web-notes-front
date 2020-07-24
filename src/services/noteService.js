5ab2a01162165a0d36480a27e198128597f81137
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-24 09:54:47 +0200

    Add request for one note by ID

--- src/services/noteService.js
+++ src/services/noteService.js
@@ -17,6 +17,18 @@ class NoteService {
                 .get(API_URL + 'users/' + id + '/notes', config)
         );
     }
+
+    getNote = async (id) => {
+        let config = {
+            headers: {
+                Authorization: authHeader()['Authorization'],
+            },
+        }
+        return (
+            await axios
+                .get(API_URL + 'notes/' + id, config)
+        );
+    }
 }
 
 export default new NoteService();
