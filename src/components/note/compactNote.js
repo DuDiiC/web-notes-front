090a1b22a1e4e0f47162ddce3eb536992f9c4217
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-24 09:57:27 +0200

    Add link to selected note inside compactNote component

--- src/components/note/compactNote.js
+++ src/components/note/compactNote.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Col } from 'react-bootstrap';
 
 import './note.css';
 
 class CompactNote extends Component {
     render() {
-        return(
+        return (
             <>
-                <Col md={6}>
-                    <div className='note-border m-3 p-3'>
-                        <span className='text-left small-text grey-text'>utworzono: <b>{this.props.note.createdAt}</b></span>
-                        <h5>{this.props.note.title}</h5>
-                        <hr className='mt-2 mb-4 p-0' />
-                        {/* {this.props.note.content.split('\n').map((line, i) => {
-                            return <p key={i} className='m-0 p-0'>{line}</p>
-                        })} */}
-                        <span className='grey-text'><b>{this.props.note.noteStatus}</b></span>
-                    </div>
+                <Col sm={6} md={4} lg={3}>
+                    <Link to={ "/notes/" + this.props.note.id } style={{ textDecoration: 'none' }}>
+                        <div className='note-border m-3 p-3'>
+                            <span className='text-left small-text grey-text'>utworzono: <b>{this.props.note.createdAt}</b></span>
+                            <h5>{this.props.note.title}</h5>
+                        </div>
+                    </Link>
                 </Col>
             </>
         )
