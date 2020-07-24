479c22411336cc4b89a687cf9a76f40be04d12d0
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-24 09:56:08 +0200

    Add a single note page

---
+++ src/components/noteSite/noteSite.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+
+class NoteSite extends Component {
+
+    render() {
+        return (
+            <Container className='py-5'>
+            </Container>
+        )
+    };
+}
+
+export default NoteSite;
