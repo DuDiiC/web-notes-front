f5d1e88c56ba7956746a7e4a675f978a81774c76
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-24 09:56:43 +0200

    Add displaying the selected note

--- src/components/noteSite/noteSite.js
+++ src/components/noteSite/noteSite.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
+import Note from '../note/note';
+import NoteService from '../../services/noteService';
 
 class NoteSite extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            note: ""
+        };
+    }
+
+    componentDidMount() {
+        NoteService.getNote(this.props.match.params.id)
+            .then((response) => {
+                const note = response.data;
+                this.setState({ note });
+            })
+            .catch(error => { console.log(error) });
+    }
 
     render() {
         return (
             <Container className='py-5'>
+            <Note note={this.state.note}/>
             </Container>
         )
     };
