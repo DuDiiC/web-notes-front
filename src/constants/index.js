6dc78c0c1d84605489610cf10fa613535f1d7183
Maciej Dudek <maciejdudek96@wp.pl>
2020-08-20 15:39:09 +0200

    Create consts file

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const APP_URL = 'https://web-notes-server.herokuapp.com/'; //HEROKU
+// const APP_URL = 'http://localhost:8080/'; //LOCAL
+
+export const API_URL = 'https://web-notes-server.herokuapp.com/api/'; //HEROKU
+// export const API_URL = 'http://localhost:8080/api/'; //LOCAL
